26885bd9820d914cfd16382d95780f86c5ac4fc5
zixi2 <zixi2@fa22-cs527-110.cs.illinois.edu>
2022-10-31 18:12:48 -0500

    add COConfiguration

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/HdfsConfiguration.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/HdfsConfiguration.java
@@ -159,4 +159,40 @@ public class HdfsConfiguration extends Configuration {
     init();
     Configuration.dumpDeprecatedKeys();
   }
+
+  public String getStackTrace() {
+    String stackTrace = " ";
+    for (StackTraceElement elem: Thread.currentThread().getStackTrace()) {
+        stackTrace = stackTrace.concat(elem.getClassName() + "\t");
+    }
+    return stackTrace;
+  }
+
+  @Override
+  public String get(String name) {
+    // LOG.warn("[CTEST][GET-PARAM] " + key + getStackTrace());
+    System.out.println("[CTEST][GET-PARAM] " + key + getStackTrace());
+    return super.get(name);
+  }
+
+  @Override
+  public String getRaw(String name) {
+    // LOG.warn("[CTEST][GET-PARAM] " + key + getStackTrace());
+    System.out.println("[CTEST][GET-PARAM] " + key + getStackTrace());
+    return super.getRaw(name);
+  }
+
+  @Override
+  public void set(String name, String value, String source) {
+    // LOG.warn("[CTEST][SET-PARAM] " + key + getStackTrace());
+    System.out.println("[CTEST][SET-PARAM] " + key + getStackTrace());
+    super.set(name, value, source);
+  }
+  
+  @Override
+  public String get(String name, String defaultValue) {
+    // LOG.warn("[CTEST][GET-PARAM] " + key + getStackTrace());
+    System.out.println("[CTEST][GET-PARAM] " + key + getStackTrace());
+    return super.get(name, defaultValue);
+  }
 }
